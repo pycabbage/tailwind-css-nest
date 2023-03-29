d4166458a40ba859c6f84372aa430bfdf9d182bb
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2023-03-30 05:30:34 +0900

    setup pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const prefix = process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY?.match(/\/(.+)$/)?.[1]}` : '';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: prefix,
 }
 
 module.exports = nextConfig
